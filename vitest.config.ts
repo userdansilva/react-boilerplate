b1bbd788bb55ca27ccdf4ea1c96a93ac18e1051f
Daniel Silva <daniel.sousasilva97@outlook.com>
2022-11-23 16:01:12 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,15 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { loadEnv } from "vite";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "happy-dom",
+    env: {
+      ...loadEnv("development", process.cwd())
+    },
   },
 });
